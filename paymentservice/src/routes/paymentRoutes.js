066e38ddd02793926b7ab4c295a22fa0d747e9e5
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-03-19 17:26:49 -0300

    feat: add JWT authentication middleware and response utility classes

--- paymentservice/src/routes/paymentRoutes.js
+++ paymentservice/src/routes/paymentRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const PaymentController = require('../controllers/paymentController');
+const {authenticateJWT} = require('../middleware/verify');
  const router = express.Router();
 
-router.post('/card-pay', (req,res)=>{
+router.post('/card-pay', authenticateJWT,(req,res)=>{
     console.log(req.body);
+    console.log(req.user);
+    return res.status(200).json({message:"Payment successful"});
 });
 router.post('/create-customer', PaymentController.createUserAccount);
 module.exports = router;
