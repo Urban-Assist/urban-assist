fed9d7159ba07d3265dfd689622da273c868962e
parthpatil01 <patilvparth15@gmail.com>
2025-03-26 20:14:11 -0300

    feat: Update CORS comments in app.js for better clarity and understanding

--- email/src/app.js
+++ email/src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
  import cors from "cors";
  const app = express();
-
+/*cors allow access to the network from the 
+cross-origin. means www.example.com can allow 
+resource usage to the www.api.com is the later
+ one is whitelisted in the former one.*/
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN, //whitelisted the given url
