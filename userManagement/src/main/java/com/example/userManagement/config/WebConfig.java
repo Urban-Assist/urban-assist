66c361a7fe4b1d3937f26a98f7256565afebb70d
parthpatil01 <patilvparth15@gmail.com>
2025-03-27 02:40:29 -0300

    fix: Update Kubernetes deployment and configuration files for user management service

--- userManagement/src/main/java/com/example/userManagement/config/WebConfig.java
+++ userManagement/src/main/java/com/example/userManagement/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
