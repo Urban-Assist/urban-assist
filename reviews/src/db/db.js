5d2bacc59dfda3aa4930a7710f0638c1a2fe2b3f
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-03-28 15:02:39 -0300

    fix: Add database port configuration to Sequelize setup

--- reviews/src/db/db.js
+++ reviews/src/db/db.js
@@ -6,9 +6,12 @@ const DB_NAME = process.env.DATABASE_NAME
 const DB_USER = process.env.DATABASE_USER
 const DB_PASSWORD = process.env.DATABASE_PASSWORD
 const DB_HOST = process.env.DATABASE_HOST
-console.log(DB_NAME,DB_USER,DB_PASSWORD,DB_HOST)
+const DB_PORT = process.env.DB_PORT;
+
+console.log(DB_NAME,DB_USER,DB_PASSWORD,DB_HOST, DB_PORT)
  const db = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
     host: DB_HOST,
+    port: DB_PORT,
     dialect: 'mysql',
      
 })
